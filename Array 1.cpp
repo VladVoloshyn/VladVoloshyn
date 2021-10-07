f404e340fdd21f29bb141c84e1dabf14897a4d0e
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2021-10-08 00:04:49 +0300

    Add files via upload

---
+++ Array 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[100];
+	int n;
+	cout << "N: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		a[i] = 1 + i * 2;
+
+	for (int i = 0; i < n; ++i)
+		cout << "a[" << i << "]=" << a[i] << "\n";
+	return 0;
+}
---
+++ Array 7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[10];
+	int n, i;
+	cout << "N: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+{
+		cout << "a[" << i << "]:";
+	cin >> a[i];
+}
+	for (int i = n - 1; i >= 0; --i)
+		cout << "a[" << i << "]=" << a[i] << "\n";
+	return 0;
+}
