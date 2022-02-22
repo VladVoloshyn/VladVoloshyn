f23acff84311bc19bc4aefcc155de3a46c524e47
VladVoloshyn <90627712+VladVoloshyn@users.noreply.github.com>
2022-02-23 00:50:12 +0200

    Add files via upload

--- Варіант 8.cpp
+++ Варіант 8.cpp
@@ -6,15 +6,17 @@ struct Node
 {
 	int number;
 	int info;
-	Node *p;
+	Node* p;
 };
-void add_car(Node **top, int number);
-void delete_car(Node **top, int number);
+
+void add_car(Node** top, int number);
+void delete_car(Node** top, int number);
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int number;
-	Node *top1 = 0;
+	Node* top1 = 0;
 	int menu = 0;
 	do
 	{
@@ -38,47 +40,49 @@ int main()
 	} while (menu != 3);
 	return 0;
 }
-void add_car(Node **top, int number)
+void add_car(Node* *top, int number)
 {
-	Node *pv = new Node;
-	pv->p = 0;
-	pv->number = number;
-	cout << "Машина с номером " << ' ' << pv->number << ' ' << "въехала на стоянку" << endl;
-	pv->info = 0;
-	if (*top == 0)
+	Node* pv = new Node;  //виділення пам'яті під новий елемент списку
+	pv->p = NULL;            //вказівник 0
+	pv->number = number;    // присвоюємо введений номер в поле структури int;
+	cout << "Машина з номером " << ' ' << pv->number << ' ' << "заехала на стоянку" << endl;
+	
+	pv->info = 0;    // інформація про те, скільки машина виїзджала
+	if (*top == 0)   //якщо структура порожня
 	{
-		*top = pv;
+		*top = pv;    //присвоюємо дану інформацію
 	}
 	else
 	{
-		pv->p = *top;
+		pv->p = *top; // добавляємо номер авто в загальний список;
 		*top = pv;
 	}
 }
-void delete_car(Node **top, int number)
+void delete_car(Node** top, int number)
 {
-	Node *tmp = *top;
-	Node *tmp2 = *top;
-	Node *pr = 0;
-	while ((tmp != 0) && (tmp->number != number))
+	Node* tmp = *top;
+	Node* tmp2 = *top;
+	Node* pr = 0;
+	while ((tmp != 0) && (tmp->number != number))   //не пустий та номер є
 	{
 		pr = tmp;
-		tmp = tmp->p;
+		tmp = tmp->p;   // присвоюємо вказівник на наступний елемент
 	}
-	if (tmp == 0)
+
+	if (tmp == 0)    // не знайшли номер в списку
 	{
 		cout << "Машины с данным номером нет на стоянке" << endl;
 	}
-	else
+	else           //знайшли номер списку
 	{
 		cout << "Машина с номером" << ' ' << tmp->number << ' ' << "выезжала со стоянки" << ' ' << tmp->info++ << ' ' << "раз" << endl;
 		while (tmp2 != tmp)
 		{
-			tmp2->info++;
-			tmp2 = tmp2->p;
+			tmp2->info++;     // \кожній машині спреду добавляється ++info
+			tmp2 = tmp2->p;   
 		}
 		if (pr != 0)
-			pr->p = tmp->p;
+			pr->p = tmp->p;   // запамятовуємо вказівник на видалений номер
 		if (tmp == *top)
 			*top = (*top)->p;
 		delete tmp;
